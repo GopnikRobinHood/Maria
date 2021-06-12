39f32f6686fe0021ceb82fc0abbd54e15499c852
GopnikRobinHood <v.wieland@live.de>
2021-06-12 23:34:42 +0200

    Connection to MongoDB

---
+++ routes/companies.js
@@ -0,0 +1,44 @@
+const express = require('express')
+const router = express.Router()
+const Company = require('../models/companies')
+
+// All Authors Route
+router.get('/', async (req, res) => {
+  let searchOptions = {}
+  if (req.query.name != null && req.query.name !== '') {
+    searchOptions.name = new RegExp(req.query.name, 'i')
+  }
+  try {
+    const companies = await Company.find(searchOptions)
+    res.render('companies/index', {
+      companies: companies,
+      searchOptions: req.query
+    })
+  } catch {
+    res.redirect('/')
+  }
+})
+
+// New Company Route
+router.get('/new', (req, res) => {
+  res.render('companies/new', { company: new Company() })
+})
+
+// Create Company Route
+router.post('/', async (req, res) => {
+  const company = new Company({
+    name: req.body.name
+  })
+  try {
+    const newCompany = await company.save()
+    // res.redirect(`authors/${newAuthor.id}`)
+    res.redirect(`companies`)
+  } catch {
+    res.render('companies/new', {
+      company: company,
+      errorMessage: 'Error creating Company'
+    })
+  }
+})
+
+module.exports = router
--- server.js
+++ server.js
@@ -2,12 +2,16 @@ if(process.env.NODE_ENV !== 'production'){
     require('dotenv').config()
 }
 
+//Require
 const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
+const bodyParser = require('body-parser')
 
 // ROUTERS
 const indexRouter = require('./routes/index')
+const companiesRouter = require('./routes/companies')
+
 // const profileRouter = require('./routes/profile')
 // const guestRouter = require('./routes/guests')
 
@@ -17,18 +21,20 @@ app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
-
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }))
 //app.use(methodOverride('_method'))
-//app.use(bodyParser.urlencoded({limit: '10mb', extended: false}))
 
+//Connect to MongoDB
 const mongoose = require('mongoose')
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true })
 const db = mongoose.connection
 db.on('error', error => console.error(error))
 db.once('open', () => console.log('Connected to Mongoose'))
 
+
 app.use('/', indexRouter)
+app.use('/companies', companiesRouter)
 
-
+//Sever listens to...
 const port = process.env.PORT
 app.listen(port, () => console.log('http://localhost:5000/'));
